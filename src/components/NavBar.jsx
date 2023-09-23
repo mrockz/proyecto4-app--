b0a0d4628627e2584d31496a0b658f1efc8ac4c4
Sergio Montero <Smontero@MacBook-Pro-de-user.local>
2023-09-23 19:17:42 -0300

    se editó ruta del texto del menú home

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,7 +12,7 @@ export const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/proyecto4-app--/">Home</Nav.Link>
             <Nav.Link as={Link} to="/proyecto4-app--/about">Nuestro Restaurant</Nav.Link>
             <Nav.Link as={Link} to="/proyecto4-app--/contacto">Contacto</Nav.Link>
             
