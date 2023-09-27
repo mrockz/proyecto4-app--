c450554d303666b8681e811348e7d4a29b699f15
Sergio Montero <Smontero@MacBook-Pro-de-user.local>
2023-09-26 22:37:49 -0300

    incoporación de nueva imagen

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -25,7 +25,7 @@ export const Home = () => {
 
       <center>
         <Card style={{ width: '48rem' }}>
-         {/* <Card.Img variant="top" src="https://img.freepik.com/foto-gratis/pintura-sushi-plato-imagen-pez_188544-12221.jpg?w=1480&t=st=1693968605~exp=1693969205~hmac=e8ce8e4bb0351a4088d303eb7c4f60f40339791ec9ad88079109d4e31f7a7a97" /> */}
+         <Card.Img variant="top" src="https://img.freepik.com/foto-gratis/pintura-sushi-plato-imagen-pez_188544-12221.jpg?w=1480&t=st=1693968605~exp=1693969205~hmac=e8ce8e4bb0351a4088d303eb7c4f60f40339791ec9ad88079109d4e31f7a7a97" /> 
           <Card.Body>
             <Card.Title>Agenda 2023 Abierta</Card.Title>
             <Card.Text>
